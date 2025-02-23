8864918c4d8da658cfeebc97b7456f6d7e6318c4
Rachel Leong <35990092+RLL24187@users.noreply.github.com>
2025-02-23 00:15:34 -0500

    added a search bar (nonfunctional) and a filter bar (functional)

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,13 +1,29 @@
 import { Row, Col } from "react-bootstrap"
 import storeItems from "../data/response.json"
 import { StoreItem } from "../components/StoreItem"
+import { useState } from "react";
 
 export function Store() {
+
+    const [query, setQuery] = useState("");
+    const filteredItems = storeItems.filter(el => (el.name.toLowerCase().includes(query.toLowerCase())));
+    filteredItems.filter(item => item.name.includes(query));
     return (
         <>
             <h1>Store</h1>
+            
+        <div className="d-flex justify-content-center align-items-center">
+            {/* <label>Filter by term: </label> */}
+            <input 
+            type="text" 
+            placeholder="Filter term" 
+            onChange={(e) =>setQuery(e.target.value)} 
+            style = {{width: "50%", fontSize: "125%", borderRadius: "25px", padding: "15px", margin: "20px"}}
+            />
+        </div>
+
             <Row md={2} xs={1} lg={3} className="g-3">
-                {storeItems.map(item => (
+                {filteredItems.map(item => (
                     <Col key={item.id}>
                         <StoreItem {...item} />
                     </Col>
