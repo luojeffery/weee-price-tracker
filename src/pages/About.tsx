1f3b2214f6eae7a62d615755162ffc88c01bc64a
Rachel Leong <35990092+RLL24187@users.noreply.github.com>
2025-02-23 00:15:18 -0500

    added a short description of project and list of contributors

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,3 +1,16 @@
 export function About() {
-    return <h1>About</h1>
+    return (
+        <div>
+
+            <h1>About</h1>
+            <h2>HopperHacks 2025 Project: StockUp⬆️</h2>
+            This app tracks and predicts product prices from the online grocery delivery service Weee! which specializes in a wide range of Asian and Hispanic products. With the help of this project, you can StockUp⬆️ fridge and your wallet!
+            <h2>Project Contributors: </h2>
+            <ul>
+                <li>Jeffery Luo</li>
+                <li>Rachel Leong</li>
+                <li>Summer Wang</li>
+            </ul>
+        </div>
+    )
 }
